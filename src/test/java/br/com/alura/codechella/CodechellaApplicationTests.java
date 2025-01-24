101eb872711bdbed8ca758a25b449295f91310ba
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2025-01-24 06:54:34 -0300

    feat: cria testes para o controle de eventos

--- src/test/java/br/com/alura/codechella/CodechellaApplicationTests.java
+++ src/test/java/br/com/alura/codechella/CodechellaApplicationTests.java
@@ -1,13 +1,40 @@
 package br.com.alura.codechella;
 
+import br.com.alura.codechella.domain.enums.TipoEvento;
+import br.com.alura.codechella.dto.EventoDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CodechellaApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void cadastraNovoEvento() {
+		EventoDto dto = new EventoDto(null, TipoEvento.SHOW, "Kiss",
+				LocalDate.parse("2025-01-01"), "Show da melhor banda que existe");
+
+		webTestClient.post().uri("/eventos").bodyValue(dto)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody(EventoDto.class).value(response -> {
+					assertNotNull(response.id());
+					assertEquals(dto.evento(), response.evento());
+					assertEquals(dto.nome(), response.nome());
+					assertEquals(dto.data(), response.data());
+
+				});
 	}
+
+
 
 }
